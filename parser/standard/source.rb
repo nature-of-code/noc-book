1214a1db72c563dce69ae56c99e678adb5bc9a5d
Rune Madsen <rune@runemadsen.com>
2011-09-30 16:49:55 -0400

    More stuff working

---
+++ parser/standard/source.rb
@@ -0,0 +1,24 @@
+plugin = {
+  :name => :source,
+	:regexp  => /^\[source,[a-z]+\]$/,
+	:handler => lambda { |lines, element|
+    
+    unless lines.next_line =~ /^-{3,}$/
+      return false
+    end
+    
+    body = ""
+    
+    while(lines.next_line) do
+      break if lines.current_line =~ /^-{3,}$/
+      body += lines.current_line
+    end
+    
+    source = AsciiElement.new(plugin[:name])
+    source.children << body
+    element.children << source
+    
+	}
+}
+
+AsciiPlugins::register(plugin)
