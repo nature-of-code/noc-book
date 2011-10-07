709c8b427bc0ca4d5a1f39fbe0e06d6d89df7814
Rune Madsen <rune@runemadsen.com>
2011-10-07 12:50:29 -0400

    bulleted lists working

---
+++ parser/standard/bulleted_list.rb
@@ -0,0 +1,22 @@
+plugin = {
+  :name => :bulleted_list,
+	:regexp  =>  /^-\s{1}/,
+	:handler => lambda { |lines, element|
+    
+    body = lines.current_line
+    
+    while(lines.shift_line) do
+      break if lines.current_line =~ /^\s*$/
+      body += lines.current_line
+    end
+    
+    body = body.split("- ")
+    body.keep_if { |child| not child.empty? }
+    
+    bulleted_list = AsciiElement.new(plugin[:name])
+    bulleted_list.children = body
+    element.children << bulleted_list
+	}
+}
+
+AsciiPlugins::register(plugin)
--- parser/standard/heading.rb
+++ parser/standard/heading.rb
@@ -1,8 +1,8 @@
 plugin = {
   :order => 1,
   :name => :heading,
-	:regexp  => /^\w+/,
-	:handler => lambda { |lines, element|
+  :regexp => /^\w+/,
+  :handler => lambda { |lines, element|
   
     # TODO: Regex should use lines size to determine how many === are needed
     unless lines.next_line =~ /^={3,}$/
@@ -15,7 +15,7 @@ plugin = {
     
     lines.shift_line
     lines.shift_line
-	}
+}
 }
 
 AsciiPlugins::register(plugin)
