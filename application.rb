1ded8a8a2301f587816e32d89a3a5a792dd32321
Rune Madsen <rune@runemadsen.com>
2012-05-05 15:40:33 -0400

    changed render api

--- application.rb
+++ application.rb
@@ -10,21 +10,37 @@ class Application < Sinatra::Base
   end
   
   get '/create_html' do
-    @document = AsciiDoc::AsciiDocument.new("templates/book.asciidoc")   
-    @document.render(:html, "templates/html", "public/results/html")
-    redirect "results/html/index.html"
+    
+    @document = AsciiDoc::AsciiDocument.new("templates/test.asciidoc")
+    
+    # Render via default templates
+    @html = @document.render(:html)
+    
+    # Render via custom templates
+    #@html = @document.render(:html, "templates/html")
+    
+    # Render to file via default templates
+    #@html = @document.render(:html, false, "public/results/html/index.html")
+    
+    # Render to file via custom templates
+    #@html = @document.render(:html, "templates/html", "public/results/html/index.html")
+    
+    #redirect "results/html/index.html"
+    
+    @html
+    
   end
   
   get '/create_pdf' do
     
     args = []
-    args << { :option => "--header-html", :value => "templates/print/views/header.html"}
+    args << { :option => "--header-html", :value => "templates/oreilly_print/views/header.html"}
     args << { :option => "--header-spacing", :value => 10} # make space between header and content
     args << { :option => "--margin-top", :value => 30} # the header spacing moves the header up, so push it down again
     
     @document = AsciiDoc::AsciiDocument.new("templates/test.asciidoc")
-    @document.render(:pdf, "templates/print", "public/results/print", args)
-    redirect "results/print/index.pdf"
+    @document.render(:pdf, "templates/oreilly_print", "public/results/oreilly_print", args)
+    redirect "results/oreilly_print/index.pdf"
   end
 
   not_found do
