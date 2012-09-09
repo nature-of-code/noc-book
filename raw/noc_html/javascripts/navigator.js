fbad55b59c6e1cba3746275b2d2c87471566ed22
Steven Klise <sklise@gmail.com>
2012-09-08 22:17:52 -0400

    Rough toc loading and collapsing

---
+++ raw/noc_html/javascripts/navigator.js
@@ -0,0 +1,37 @@
+(function() {
+
+  addExpandLinks = function() {
+    // Add expand links
+    var chapters;
+    chapters = $('#toc-holder>ul>li');
+    chapters.each(function() {
+      $(this).append(' <a href="#" class="expand">↓</a>')
+    });
+  }
+
+  expandChapter = function() {
+    $('.expand').click(function() {
+      $(this).closest('li').next().toggle();
+      return false;
+    });
+  }
+
+  setupTOC = function() {
+    addExpandLinks();
+    expandChapter();
+  }
+
+  $(function() {
+    var $toc;
+
+    $toc = $('#toc-holder');
+
+    // Load in the TOC
+    $.ajax('toc.html').done(function(data) {
+      $toc.html(data);
+      setupTOC();
+    });
+
+  });
+
+}).call(this);
