5d991efff029a40e73dcb0a7a44f6674f7713c71
Steven Klise <sklise@gmail.com>
2012-10-05 16:22:10 -0400

    Properly reset sketches

--- raw/noc_html/javascripts/sketchControls.js
+++ raw/noc_html/javascripts/sketchControls.js
@@ -1,10 +1,20 @@
 $(function(){
+  // Reload the sketch from sources. Also set paused to false
   $('.reset').click(function() {
     var controls = $(this).closest('.sketch-controls');
     var pjsID = $(controls).prev().attr('id');
-    var pjs = Processing.getInstanceById(pjsID);
-    pjs.setup();
-    pjs.redraw();
+    var sketch = document.getElementById(pjsID);
+
+    // Adapted from Processingjs lazyloading extension.
+    // https://github.com/processing-js/processing-js/blob/master/extensions/processing-lazyload.js
+    var processingSources = sketch.getAttribute('data-processing-sources');
+    var filenames = processingSources.split(' ');
+    for (var j = 0; j < filenames.length;) {
+      if (filenames[j]) { j++; }
+      else { filenames.splice(j, 1); }}
+    Processing.loadSketchFromSources(sketch, filenames);
+
+    $(controls).find('.pause').data()['paused'] = 'false';
   });
 
   $('.pause').click(function() {
