9df5204af6c2d665b59f4a41057cd3cafb90cfd2
Steven Klise <sklise@gmail.com>
2012-10-13 17:49:48 -0400

    Remove setup toc code

--- raw/noc_html/javascripts/navigator.js
+++ raw/noc_html/javascripts/navigator.js
@@ -1,26 +1,5 @@
 (function() {
 
-  addExpandLinks = function() {
-    // Add expand links
-    var chapters;
-    chapters = $('#toc-list>ul>li');
-    chapters.each(function() {
-      $(this).append(' <span class="expand">l</span>')
-    });
-  }
-
-  expandChapter = function() {
-    $('.expand').click(function() {
-      $(this).closest('li').next().toggle();
-      return false;
-    });
-  }
-
-  setupTOC = function() {
-    addExpandLinks();
-    expandChapter();
-  }
-
   $(function() {
     var $toc;
 
@@ -28,13 +7,6 @@
 
     $('#toc-list').fixed({'top':'8'});
     $('#nav-bar-wrap').fixed({'top':'8'});
-
-    // Load in the TOC
-    $.ajax('toc.html').done(function(data) {
-      $toc.append(data);
-      setupTOC();
-    });
-
   });
 
 }).call(this);
