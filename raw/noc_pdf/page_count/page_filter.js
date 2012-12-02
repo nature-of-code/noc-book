e8ca807dab42097860ca369f0283ca0594da87de
Rune Skjoldborg Madsen <rune@runemadsen.com>
2012-12-01 23:24:13 -0500

    fixing page bug in index

--- raw/noc_pdf/page_count/page_filter.js
+++ raw/noc_pdf/page_count/page_filter.js
@@ -15,50 +15,69 @@ function rewriteIndex()
 
 function removeDuplicates(li)
 {
+    var index_term_name = children_of_type_and_class(li, "span", "index-term-name")[0];
+    if(!index_term_name)  return;
+
+    Log.info("---> Checking for indexterm: " + index_term_name.innerHTML);
+
     var text = "";
     var lastPage;
+    var delete_indexes = [];
 
-    var links = children_of_type(li, "a");//li.getElementsByTagName("a");
+    var links = children_of_type_and_class(li, "span", "index-link");
 
+    // find elements
     for (var i = 0; i < links.length; ++i)
     {
-        var link = links[i];
+        var link = children_of_type_and_class(links[i], "a", "index-term-link")[0];
         var href = link.getAttribute("href");
         var page = refs[href];
 
+        Log.info("Comparing Page " + page + " to last page " + lastPage);
         if(page == lastPage)
         {
-            link.style.display = "none";
-
-            // if there's a comma behind this, remove it
-            var prevSibling = get_previoussibling(link);
-            if(prevSibling && prevSibling.className == "comma")
-            {
-                prevSibling.style.display = "none";
-            }
+          //Log.info("SHOULD DELETE: " + links[i].innerHTML);
+          //Log.info("Duplicate!!!");
+          delete_indexes.push(i);
         }
 
         lastPage = page;
+    }
+
+    //Log.info("INDEXES TO DELETE " + delete_indexes.toString());
+
+    // delete the elements
+    for(var i = links.length - 1; i >= 0; i--)
+    {
+        //Log.info("I IS: " + i);
+        if(delete_indexes.indexOf(i) > -1)
+        {
+            var link = links[i];
+
+            //Log.info("Deleting " + link.innerHTML);
+            // delete container of link
+            link.parentNode.removeChild(link);
+        }
     }
 }
 
 /* Get Children With Class Name
 ---------------------------------------------------------------------- */
 
-function children_of_type(n, nodeName)
+function children_of_type_and_class(n, nodeName, className)
 {
-    var classChildren = [];
+    var selected = [];
 
     for(var i = 0; i < n.childNodes.length; i++)
     {
         var child = n.childNodes[i];
 
-        if(child.nodeName == nodeName)
+        if(child.nodeName == nodeName && child.className == className)
         {
-            classChildren.push(child);
+            selected.push(child);
         }
     }
-    return classChildren;
+    return selected;
 }
 
 
