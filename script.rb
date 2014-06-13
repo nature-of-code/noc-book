701cff34eaa249d5f2162fe38f74f6fe455e5e90
Rune Madsen <rune@runemadsen.com>
2014-06-13 17:31:36 -0400

    changing a bunch of the files

--- script.rb
+++ script.rb
@@ -4,11 +4,10 @@ folder = "./raw"
 
 Dir.foreach(folder) do |file|
   if File.extname(file) == ".asc"
-    Asciidoctor.render_file("#{folder}/#{file}", 
-      :safe => :safe,
-      :header_footer => false,
-      :in_place => true,
-      :template_dir => "/Users/runemadsen/Projects/OReilly/orm-atlas-cookbooks/repositories/asciidoctor-htmlbook/htmlbook"
-    )
+    template_dir = "/Users/runemadsen/Projects/OReilly/orm-atlas-cookbooks/repositories/asciidoctor-htmlbook/htmlbook"
+    file_content = File.open("#{folder}/#{file}").read
+    base_dir = File.dirname("#{folder}/#{file}")
+    content = Asciidoctor.render(file_content, :safe => :safe, :in_place => true, :base_dir => base_dir, :template_dir => template_dir)
+    File.open("#{folder}/#{File.basename(file, ".asc")}.html", 'w') {|f| f.write(content) }
   end
 end
