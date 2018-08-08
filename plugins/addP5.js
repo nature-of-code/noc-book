f939a504ed20d414c6c998976584b427c484cf0f
nelliemckesson <nelliemckesson@gmail.com>
2018-08-08 13:17:50 -0700

    adding more robust support for processing examples

--- plugins/addP5.js
+++ plugins/addP5.js
@@ -20,11 +20,34 @@ Plugin.prototype = {
     stream = stream.pipe(through.obj(function(file, enc, cb) {
         if(!file.$el) file.$el = cheerio.load(file.contents.toString());
           // Loop through all figures to replace with iframes
-          file.$el('figure[data-pde-sketch^="http"]').each(function(i, el) { 
+          file.$el('figure[data-p5-sketch^="http"]').each(function(i, el) { 
             var jel = file.$el(this);
-            var source = jel.attr('data-pde-sketch');
-            jel.after('<iframe src="' + source + '" style="width:640px;height:360px;"></iframe>');
-            //jel.attr("style","display:none;");
+            //<p class="caption">angleVel = 0.05</p>
+            var source = jel.attr('data-p5-sketch');
+            if (jel.attr('class') && jel.attr('class').indexOf('two-col') > -1) {
+              var newel = '<div class="two-col"><iframe class="two-col" data-src="' + source + '" width="640" height="360" frameborder="0" allowfullscreen></iframe></div>';
+              if (jel.prev().attr('class') && jel.prev().attr('class').indexOf('two-col-wrapper') > -1) {
+                jel.prev().find('div.two-col-container').append(newel);
+              } else {
+                jel.before('<div class="two-col-wrapper"><div class="two-col-container">' + newel + '</div></div>');
+              }
+            } else if (jel.attr('class') && jel.attr('class').indexOf('three-col') > -1) {
+              var newel = '<div class="three-col"><iframe class="three-col" data-src="' + source + '" width="640" height="360" frameborder="0" allowfullscreen></iframe></div>';
+              if (jel.prev().attr('class') && jel.prev().attr('class').indexOf('three-col-wrapper') > -1) {
+                jel.prev().find('div.three-col-container').append(newel);
+              } else {
+                jel.before('<div class="three-col-wrapper"><div class="three-col-container">' + newel + '</div></div>');
+              }
+            } else {
+              var newel = '<div class="one-col-wrapper"><iframe data-src="' + source + '" width="640" height="360" frameborder="0" allowfullscreen></iframe></div>';
+              jel.before(newel);
+            }
+            // check for a caption; if found, add to new element
+            var caption = jel.find('figcaption');
+            if (caption.length > 0 && /\S+/.test(caption.text())) {
+              var prevframe = jel.prev().find('iframe').last();
+              prevframe.after('<p class="caption">' + caption.html() + '</p>');
+            }
             jel.empty();
             jel.remove();
           });
